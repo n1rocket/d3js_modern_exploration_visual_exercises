bcd0fc86b31adead119f4f039c58ef519ade3636
Victor Manuel Garcia Sanabria <vik000@gmail.com>
2022-07-19 08:10:33 +0100

    Ejercicio final

--- 01/ejercicio1/main.js
+++ 01/ejercicio1/main.js
@@ -1,5 +1,7 @@
 const height = 500
 const width = 800
+const margin = 50
+
 const colors = ["#440154", "#482475", "#414487", "#355f8d", "#2a788e", "#21908d", "#22a884", "#42be71", "#7ad151", "#bddf26", "#bddf26"]
 
 const svg = d3.select("#chart").append("svg").attr("width", width).attr("height", height)
@@ -13,7 +15,6 @@ function addCircle(){
         .attr("r", (5 + (Math.random() * 30)))
         .attr("fill", colors[Math.floor(Math.random() * colors.length)])
         .on("click", function() {
-            // console.log(this)
             this.remove()
         })
 }
